4243f9328ede0e4c1b331b912ec866e3372831c2
Andrusi4ka <Andrusi4ka1985@gmail.com>
2025-10-26 04:36:52 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './index.html';
+import './styles/fonts.scss';
+import './styles/style.scss';
+import './styles/tailwind.css';
+
+import 'prismjs';
+import 'prismjs/themes/prism-okaidia.css';
+import 'prismjs/components/prism-css.min.js';
+import 'prismjs/components/prism-javascript.min.js';
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, './README.md'), to: 'README.md'},
+        { from: path.resolve(__dirname, 'src/images'), to: 'images' },
+      ],
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      watch: true,
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    port: 8080,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset',
+        parser: { dataUrlCondition: { maxSize: 8 * 1024 } },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ],
+  },
+};
